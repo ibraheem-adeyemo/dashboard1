7e46c8d460457ecaa1ed7ff9d706891972f527c9
Ibraheem Adeyemo <ibraheem.adeyemo@interswitchgroup.com>
2025-11-18 21:53:41 +0100

    fixed dropdown issue

--- src/components/ui/dropdown/Dropdown.tsx
+++ src/components/ui/dropdown/Dropdown.tsx
@@ -27,7 +27,7 @@ const Dropdown = ({ trigger, children, onClick, align = "right" }: DropdownProps
         <MenuItems
           className={`absolute ${
             align === "right" ? "right-0" : "left-0"
-          } mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black/5 focus:outline-none`}
+          } mt-2 w-56 origin-top-right rounded-md bg-white dark:bg-gray-800 shadow-lg ring-1 ring-black/5 focus:outline-none z-10`}
         >
           {children}
         </MenuItems>
